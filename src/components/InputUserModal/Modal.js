5c5b99debb0a36acb3781249083563b9ea613d4a
Andra Putra <ricardoandra17@gmail.com>
2023-12-21 09:08:06 +0700

    add secret service

--- src/components/InputUserModal/Modal.js
+++ src/components/InputUserModal/Modal.js
@@ -111,7 +111,7 @@ export default function InputUserModal({ open, isError, onClose, InquiryUserList
   return (
     <>
       <Modal open={open && !isDelete} onClose={onClose} style={modalContainerStyle}>
-        <Container maxWidth="sm">
+        <Container maxWidth="md">
           <div style={modalContentStyle}>
             <Stack
               direction="column"
@@ -123,102 +123,106 @@ export default function InputUserModal({ open, isError, onClose, InquiryUserList
               <Typography variant="h3" style={modalTitleStyle}>
                 {initialData ? 'Change User Data' : 'Create New User'}
               </Typography>
-              <Stack direction="column" spacing={3}>
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <Iconify icon={'bxs:id-card'} width={50} sx={{ color: 'primary.main' }} />
-                  <TextField
-                    id='user_id'
-                    name="user_id"
-                    label="Username"
-                    value={userData.user_id || ''}
-                    onChange={handleUserData}
-                  />
-                </Stack>
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <Iconify icon={'ic:round-person-add-alt-1'} width={50} sx={{ color: 'primary.main' }} />
-                  <TextField
-                    id='full_name'
-                    name="full_name"
-                    label="Full Name"
-                    value={userData.full_name || ''}
-                    onChange={handleUserData}
-                  />
-                </Stack>
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <Iconify icon={'ic:round-person-add-alt-1'} width={50} sx={{ color: 'primary.main' }} />
-                  <TextField
-                    id='short_name'
-                    name="short_name"
-                    label="Short Name"
-                    value={userData.short_name || ''}
-                    onChange={handleUserData}
-                  />
-                </Stack>
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <Iconify icon={'ic:round-person-add-alt-1'} width={50} sx={{ color: 'primary.main' }} />
-                  <Stack direction="column">
-                    <Select
-                      labelId="level-label"
-                      style={{ width: '200px' }}
-                      id="level_id"
-                      name="level_id"
-                      value={userData.level_id || '1'}
-                      label="Level"
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Stack direction="column" spacing={3}>
+                  <Stack direction="row" spacing={2} alignItems="center">
+                    <Iconify icon={'bxs:id-card'} width={50} sx={{ color: 'primary.main' }} />
+                    <TextField
+                      id='user_id'
+                      name="user_id"
+                      label="Username"
+                      value={userData.user_id || ''}
                       onChange={handleUserData}
-                    >
-                      <MenuItem value={'1'}>User</MenuItem>
-                      <MenuItem value={'0'}>Admin</MenuItem>
-                    </Select>
-                    <FormHelperText>&nbsp; Account Level</FormHelperText>
+                    />
+                  </Stack>
+                  <Stack direction="row" spacing={2} alignItems="center">
+                    <Iconify icon={'ic:round-person-add-alt-1'} width={50} sx={{ color: 'primary.main' }} />
+                    <TextField
+                      id='full_name'
+                      name="full_name"
+                      label="Full Name"
+                      value={userData.full_name || ''}
+                      onChange={handleUserData}
+                    />
+                  </Stack>
+                  <Stack direction="row" spacing={2} alignItems="center">
+                    <Iconify icon={'ic:round-person-add-alt-1'} width={50} sx={{ color: 'primary.main' }} />
+                    <TextField
+                      id='short_name'
+                      name="short_name"
+                      label="Short Name"
+                      value={userData.short_name || ''}
+                      onChange={handleUserData}
+                    />
                   </Stack>
                 </Stack>
-                
-                {!initialData && 
-                  <><Stack direction="row" spacing={2} alignItems="center">
-                    <Iconify icon={'mdi:password-check'} width={50} sx={{ color: 'primary.main' }} />
-                    <TextField
-                      id='password'
-                      name="password"
-                      label="Password"
-                      type={showPassword ? 'text' : 'password'}
-                      value={userData.password || ''}
-                      onChange={handleUserData}
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                              <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                            </IconButton>
-                          </InputAdornment>
-                        ),
-                      }} />
-                  </Stack><Stack direction="row" spacing={2} alignItems="center">
+                <Stack direction="column" spacing={3}>
+                  <Stack direction="row" spacing={2} alignItems="center">
+                    <Iconify icon={'eos-icons:admin'} width={50} sx={{ color: 'primary.main' }} />
+                    <Stack direction="column">
+                      <Select
+                        labelId="level-label"
+                        style={{ width: '200px' }}
+                        id="level_id"
+                        name="level_id"
+                        value={userData.level_id || '1'}
+                        label="Level"
+                        onChange={handleUserData}
+                      >
+                        <MenuItem value={'1'}>User</MenuItem>
+                        <MenuItem value={'0'}>Admin</MenuItem>
+                      </Select>
+                      {/* <FormHelperText>&nbsp; Account Level</FormHelperText> */}
+                    </Stack>
+                  </Stack>
+                  
+                  {!initialData && 
+                    <><Stack direction="row" spacing={2} alignItems="center">
                       <Iconify icon={'mdi:password-check'} width={50} sx={{ color: 'primary.main' }} />
-                      <Stack direction="column">
-                        <TextField
-                          id='confirmPassword'
-                          name="confirmPassword"
-                          label="Confirm Password"
-                          type={showConfirmPassword ? 'text' : 'password'}
-                          value={confirmPassword || ''}
-                          onChange={handleConfirmPassword}
-                          InputProps={{
-                            endAdornment: (
-                              <InputAdornment position="end">
-                                <IconButton onClick={() => setShowConfirmPassword(!showConfirmPassword)} edge="end">
-                                  <Iconify icon={showConfirmPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                                </IconButton>
-                              </InputAdornment>
-                            ),
-                          }} />
-                        {!isMatchPassword && (
-                          <FormHelperText style={{ color: 'red' }}>
-                            &nbsp; Password do not match
-                          </FormHelperText>
-                        )}
-                      </Stack>
-                    </Stack></>
-                }
+                      <TextField
+                        id='password'
+                        name="password"
+                        label="Password"
+                        type={showPassword ? 'text' : 'password'}
+                        value={userData.password || ''}
+                        onChange={handleUserData}
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                                <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                              </IconButton>
+                            </InputAdornment>
+                          ),
+                        }} />
+                    </Stack><Stack direction="row" spacing={2} alignItems="center">
+                        <Iconify icon={'mdi:password-check'} width={50} sx={{ color: 'primary.main' }} />
+                        <Stack direction="column">
+                          <TextField
+                            id='confirmPassword'
+                            name="confirmPassword"
+                            label="Confirm Password"
+                            type={showConfirmPassword ? 'text' : 'password'}
+                            value={confirmPassword || ''}
+                            onChange={handleConfirmPassword}
+                            InputProps={{
+                              endAdornment: (
+                                <InputAdornment position="end">
+                                  <IconButton onClick={() => setShowConfirmPassword(!showConfirmPassword)} edge="end">
+                                    <Iconify icon={showConfirmPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                  </IconButton>
+                                </InputAdornment>
+                              ),
+                            }} />
+                          {!isMatchPassword && (
+                            <FormHelperText style={{ color: 'red' }}>
+                              &nbsp; Password do not match
+                            </FormHelperText>
+                          )}
+                        </Stack>
+                      </Stack></>
+                  }
+                </Stack>
               </Stack>
 
               <Stack direction="row" spacing={3}>
