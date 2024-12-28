4fcae341a7f88cabd78afc7171c0a474682cde19
Andra Putra <ricardoandra17@gmail.com>
2024-12-28 17:06:00 +0700

    route

--- src/routes.js
+++ src/routes.js
@@ -76,18 +76,6 @@ export default function Router() {
       path: '',
       element: isLoggedIn ? <DashboardLayout /> : <Navigate to="/login" />,
     },
-    {
-      element: <SimpleLayout />,
-      children: [
-        { element: <Navigate to="/dashboard/app" />, index: true },
-        { path: '404', element: <Page404 /> },
-        { path: '/*', element: <Navigate to="404" /> },
-      ],
-    },
-    {
-      path: '/*',
-      element: <Navigate to="/404" replace />,
-    },
   ]);
 
   return routes;
