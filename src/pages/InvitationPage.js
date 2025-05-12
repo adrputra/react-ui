69f02d045c48d416e5265e4a38e12bebe9f0f4e1
Andra Putra <oktaviandra.peditya@paracorpgroup.com>
2025-05-12 16:16:33 +0700

    fix error

--- src/pages/InvitationPage.js
+++ src/pages/InvitationPage.js
@@ -279,7 +279,7 @@ export default function InvitationPage() {
                           break;
 
                         default:
-                          statusColor = 'yellow';
+                          statusColor = 'warning';
                           break;
                       }
 
